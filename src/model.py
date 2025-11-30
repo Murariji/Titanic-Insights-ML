5e5beac06d611c4feca514387b03b8f55e52089e
Murariji <143028323+Murariji@users.noreply.github.com>
2025-12-01 00:27:55 +0530

    Added model building and saving functions

---
+++ src/model.py
@@ -0,0 +1,19 @@
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+from pathlib import Path
+
+# model will be saved in /models/model.pkl
+MODEL_PATH = Path(__file__).resolve().parents[1] / "model.pkl"
+
+def build_model():
+    return RandomForestClassifier(
+        n_estimators=150,
+        random_state=42,
+        n_jobs=-1
+    )
+
+def save_model(model):
+    joblib.dump(model, MODEL_PATH)
+
+def load_model():
+    return joblib.load(MODEL_PATH)
